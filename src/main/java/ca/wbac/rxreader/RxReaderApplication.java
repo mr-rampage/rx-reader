967b186f3e2a8df4c3462972788f14c0b0eb2e7b
mr-rampage <readonly@che.wbac.ca>
2018-05-30 21:05:09 -0400

    chore(init)

---
+++ src/main/java/ca/wbac/rxreader/RxReaderApplication.java
@@ -0,0 +1,12 @@
+package ca.wbac.rxreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxReaderApplication.class, args);
+	}
+}
